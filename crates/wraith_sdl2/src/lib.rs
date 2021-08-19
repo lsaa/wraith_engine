551462ee1dd87bfadc7be8848e2b3e35d0beb6f7
lsaa <lsaadev.personal@gmail.com>
2021-08-19 18:19:43 -0300

    poggy branch

--- crates/wraith_sdl2/src/lib.rs
+++ crates/wraith_sdl2/src/lib.rs
@@ -86,8 +86,8 @@ pub fn save_texture_to_file(canvas: &mut Canvas<Window>, path: &Path, texture: &
 	canvas.with_texture_canvas(&mut target, |tex| {
 		tex.copy(texture, None, None).unwrap();
 		let mut pixels_vec = tex.read_pixels(None, format).unwrap();
-		let pitch = (format.byte_size_per_pixel() * (w as usize * h as usize)) as u32;
-		let surface = Surface::from_data(&mut pixels_vec, w, h, pitch / 4, format).unwrap();
+		let pitch = (format.byte_size_per_pixel() * w as usize) as u32;
+		let surface = Surface::from_data(&mut pixels_vec, w, h, pitch, format).unwrap();
 		let _= surface.save(path);
 	}).unwrap();
 	Ok(())
